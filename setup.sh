74d1ecdd4ca49caeddb55567db1822b6d81cfd3f
Yucao42 <cqcy3210@gmail.com>
2020-05-26 15:26:03 -0400

    add cpp indent

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 here=$PWD/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 
+# Formatter and nerdtree utils
 cp .vimrc ${HOME}/
 vim +PluginInstall +qall
 cd ${HOME}/.vim/bundle/YouCompleteMe/in
 python3 install.py
+
+# Use google-vim for cpp indent
+mkdir -p ${HOME}/.vim/indent
+cd ${HOME}/.vim/indent
+wget https://vim8.org/scripts/download_script.php?src_id=14772 -O cpp.vim
 cd $here
